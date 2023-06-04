640049e3ddf7141a25c0a54a5bfcf9648422ac9e
mac <zayns1914@gmail.com>
2023-06-04 14:13:10 +0200

    update

--- myApp.js
+++ myApp.js
@@ -1,40 +1,33 @@
-let express = require('express');
+require('dotenv').config();
+
+let express = require("express");
 let app = express();
 
+absolutePath = __dirname + "/views/index.html";
+absolutePath2 = __dirname + "/public";
+publicPath = "/public";
+
+app.get("/", function (req, res) {
+  res.sendFile(absolutePath);
+});
+
+app.use(publicPath, express.static(__dirname + publicPath));
 
 
 
-console.log("hello");
-console.log("hello2");
-console.log("hello");
+app.get("/json", function (req, res) {
+    var response = { message: "Hello json" };
+    console.log(response.message.toUpperCase());
 
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+      response.message = response.message.toUpperCase();
+      console.log(response.message.toUpperCase());
+    }
+  
+    res.json(response);
+    console.log(response.message.toUpperCase());
 
+  });
+  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
