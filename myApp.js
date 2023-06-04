bcab72b20bddd1fc160f55e6c211119ef86f4382
mac <zayns1914@gmail.com>
2023-06-04 17:30:23 +0200

    update

--- myApp.js
+++ myApp.js
@@ -6,10 +6,31 @@ let app = express();
 absolutePath = __dirname + "/views/index.html";
 absolutePath2 = __dirname + "/public";
 publicPath = "/public";
+console.log("response.message.toUpperCase()");
+
 
 app.get("/", function (req, res) {
   res.sendFile(absolutePath);
 });
+
+
+app.use(function(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
+
+
+app.get('/now', function(req, res, next) {
+  req.now = new Date().toString();  // Hypothetical synchronous operation
+  console.log(`This is req.now ${req.now}`);
+  next();
+}, function(req, res) {
+  res.json({ time: req.now })
+  // console.log(`This is req.now ${res.json({ time: req.now })}`);
+});
+
+
 
 app.use(publicPath, express.static(__dirname + publicPath));
 
@@ -29,5 +50,6 @@ app.get("/json", function (req, res) {
 
   });
   
+
 
 module.exports = app;
